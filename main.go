8905b98c0095305fdcca656201da1f9329f5db1f
kmoon <1218moonlight@gmail.com>
2019-01-16 13:40:00 +0900

    서버 ping 체크 サーバーのPINGをチェック

--- main.go
+++ main.go
@@ -4,35 +4,51 @@ import (
 	"github.com/andlabs/ui"
 	_ "github.com/andlabs/ui/winmanifest"
 	"log"
+	"github.com/sparrc/go-ping"
+	"fmt"
 )
 
-var mainWin *ui.Window
+type window struct {
+	main       *ui.Window
+	title      string
+	width      int
+	height     int
+	hasMenubar bool
+}
 
 func main() {
 	ui.Main(GUI)
 }
 
 func GUI() {
-	mainWin = ui.NewWindow("Electrumx-Dashboard", 600, 600, false)
+	win := window{
+		main:       nil,
+		title:      "Electrumx-Dashboard",
+		width:      600,
+		height:     600,
+		hasMenubar: false,
+	}
 
-	mainWin.OnClosing(func(window *ui.Window) bool {
+	win.main = ui.NewWindow(win.title, win.width, win.height, win.hasMenubar)
+
+	win.main.OnClosing(func(window *ui.Window) bool {
 		ui.Quit()
 		log.Println("OnClosing")
 		return true
 	})
 	ui.OnShouldQuit(func() bool {
-		mainWin.Destroy()
+		win.main.Destroy()
 		log.Println("OnShouldQuit")
 		return true
 	})
 
 	tab := ui.NewTab()
-	mainWin.SetChild(tab)
-	mainWin.SetMargined(true)
+	win.main.SetChild(tab)
+	win.main.SetMargined(true)
 
 	tab.Append("Server", mainTab())
 
-	mainWin.Show()
+	win.main.Show()
 }
 
 func mainTab() ui.Control {
@@ -40,5 +56,24 @@ func mainTab() ui.Control {
 	vbox.SetPadded(true)
 
 	vbox.Append(ui.NewLabel("Hello, World!"), false)
+
+	c := make(chan ping.Statistics)
+	go serverPing(c)
+	n := <- c
+	vbox.Append(ui.NewLabel(fmt.Sprintln(n)), false)
+
 	return vbox
+}
+
+func serverPing(c chan ping.Statistics) {
+	pinger, err := ping.NewPinger("URL")
+	pinger.SetPrivileged(true)
+	if err != nil {
+		panic(err)
+	}
+
+	pinger.Count = 3
+	pinger.Run()                 // blocks until finished
+	stats := pinger.Statistics() // get send/receive/rtt stats
+	c <- *stats
 }
