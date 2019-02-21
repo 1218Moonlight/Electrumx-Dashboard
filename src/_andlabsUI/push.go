9de9c619b51ee9f34136e279d27aa92557358399
kmoon <1218moonlight@gmail.com>
2019-02-21 19:25:36 +0900

    checkError 수정... checkError 修正。。。

--- src/_andlabsUI/push.go
+++ src/_andlabsUI/push.go
@@ -11,8 +11,8 @@ var pingBool bool = false
 
 func serverPing(url string, laber *ui.Label) {
 	pinger, err := ping.NewPinger(url)
-	pinger.SetPrivileged(true)
 	if checkError(err, false) {return}
+	pinger.SetPrivileged(true)
 	pinger.Timeout = time.Duration(time.Second * 1)
 	pinger.Count = 1
 	pinger.Run()
